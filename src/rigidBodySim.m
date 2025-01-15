32c5993bc499b28409a0044d657e1918af78eba2
ogahector <ogahector@gmail.com>
2025-01-15 10:19:37 +0000

    matlab's example of a bot w/ gui

--- src/rigidBodySim.m
+++ src/rigidBodySim.m
@@ -2,19 +2,29 @@ clear;
 close all;
 
 
-rob = rigidBodyTree();
+rob = rigidBodyTree("DataFormat","row");
 
 link1 = rigidBody('link1');
-jnt1 = rigidBodyJoint('joint1', 'fixed');
-link1.Joint = jnt1;
+link2 = rigidBody('link2');
+link3 = rigidBody('link3');
+
+
+jnt1 = rigidBodyJoint('joint1', 'revolute');
+jnt2 = rigidBodyJoint('joint2', 'revolute');
+jnt3 = rigidBodyJoint('joint3', 'revolute');
 
 jnt1.setFixedTransform(makehgtform("translate", [1 0 0]))
+jnt2.setFixedTransform(makehgtform("translate", [0 1 0]))
+jnt3.setFixedTransform(makehgtform("translate", [0 0 1]))
 
+link1.Joint = jnt1;
+link2.Joint = jnt2;
+link3.Joint = jnt3;
 
 rob.addBody(link1, 'base')
-rob.showdetails;
+rob.addBody(link2, 'link1')
+rob.addBody(link3, 'link2')
 
-% figure(Name= 'Interactive GUI')
-% hold on
-% gui = interactiveRigidBodyTree(rob, MarkerScaleFactor=0.5);
+rob.showdetails;
 rob.show;
+% gui = interactiveRigidBodyTree(rob, MarkerScaleFactor=0.5);
