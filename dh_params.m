040bd09d25198b4bc778428c3e9c4061d76c968c
ogahector <ogahector@gmail.com>
2025-01-21 17:44:38 +0000

    progress on ObjectPose - should we rename it?

---
+++ dh_params.m
@@ -0,0 +1,14 @@
+clc;
+clear;
+close all;
+
+%% 
+
+syms theta1 theta2 theta3 theta4 theta5
+
+DH1 = [0 0 0.077 theta1];
+DH2 = [90 0 0 theta2];
+DH3 = [0 0.130 0 theta3];
+DH4 = [0 0.124 0 theta4];
+
+DH5 = [0 .126 0 theta5];
--- include/ObjectPose.m
+++ include/ObjectPose.m
@@ -2,24 +2,28 @@
 % i have no idea if this is the right direction please modify 
 classdef ObjectPose
     properties (Access = public)
-    name {mustBeNonzeroLengthText};
-    world_frame {mustBeFloat};
-    frame {mustBeFloat};
-    % R {mustBeFloat};
-    P {mustBeFloat};
-    % T {mustBeFloat};
+        name {mustBeNonzeroLengthText};
+        world_frame {mustBeFloat};
+        frame {mustBeFloat};
+        % R {mustBeFloat};
+        P {mustBeFloat};
+        % T {mustBeFloat};
+    
+        lib_name {mustBeText};
+        SERVO_ID {mustBeText};
+        PROTOCOL_VERSION {mustBeNumeric};
+        port_num {mustBeText};
 
-    lib_name {mustBeText};
-    SERVO_ID {mustBeText};
-    PROTOCOL_VERSION {mustBeNumeric};
-    port_num {mustBeText};
+        parent = ObjectPose.empty; %?????????????????????
+        child = ObjectPose.empty;  %?????????????????????
+
+        ADDR = struct();
     end
 
     methods
-        % function obj = ObjectPose(name, other, world_frame_name, frame, P_initial, offset)
         function obj = ObjectPose(name, world_frame_ptr, frame, P_initial, ...
                                   SERVO_ID, PROTOCOL_VERSION, DEVICENAME, BAUDRATE)
-        % function obj = ObjectPose(varargin)
+
             if ~libisloaded('shrlibsample')
                 addpath(fullfile(matlabroot,'extern','examples','shrlib'))
                 loadlibrary('shrlibsample')
@@ -38,9 +42,17 @@ classdef ObjectPose
             obj.world_frame = world_frame_ptr;
             obj.frame = frame;
             obj.P = P_initial;
+
+
+            obj.ADDR.TORQUE_ENABLE       = 64;           
+            obj.ADDR.GOAL_POSITION       = 116; 
+            obj.ADDR.PRESENT_POSITION    = 132; 
+            obj.ADDR.OPERATING_MODE      = 11;
+            obj.ADDR.MAX_POS            = 48;
+            obj.ADDR.MIN_POS             = 52;
         end
 
-        function obj = servoInit(obj)
+        function obj = servoInit(obj) 
             startup_load_libraries();
             ...
         end
@@ -61,6 +73,42 @@ classdef ObjectPose
 
         function obj = updateWorldFrame(obj, new_WF)
             obj.world_frame = new_WF;
+        end
+
+%% Dynamixel - related methods
+        function obj = disableTorque(obj)
+            write1ByteTxRx(obj.port_num, obj.PROTOCOL_VERSION, ...
+                obj.SERVO_ID, obj.ADDR.TORQUE_ENABLE, 0);
+        end
+
+        function obj = enableTorque(obj)
+            write1ByteTxRx(obj.port_num, obj.PROTOCOL_VERSION, ...
+                obj.SERVO_ID, obj.ADDR.TORQUE_ENABLE, 1);
+        end
+
+        function obj = setRotationLimitsPulse(obj, min, max)
+            obj.disableTorque()
+
+            write4ByteTxRx(obj.port_num, obj.PROTOCOL_VERSION, ...
+                obj.SERVO_ID, obj.ADDR.MIN_POS, min);
+
+            write4ByteTxRx(obj.port_num, obj.PROTOCOL_VERSION, ...
+                obj.SERVO_ID, obj.ADDR.MAX_POS, max);
+        end
+
+        function obj = setRotationLimitsDeg(obj, min, max)
+            obj.disableTorque()
+
+            obj.setRotationLimitsPulse(obj, dynDeg2pulse(min), dynDeg2pulse(max));
+        end
+
+        function obj = moveToPulse(obj, target)
+            write4ByteTxRx(obj.port_num, obj.PROTOCOL_VERSION, ...
+                obj.SERVO_ID, obj.ADDR.GOAL_POSITION, target)
+        end
+
+        function obj = moveToDeg(obj, target)
+            obj.moveToPulse(dynDeg2pulse(target));
         end
     end
 end
